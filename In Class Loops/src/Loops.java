efe84d7ef21bd65cb059152cb7642ff277b712f8
Brad D. Messner <bmessner@setonhill.edu>
2022-09-21 22:19:16 -0400

    :)

---
+++ In Class Loops/src/Loops.java
@@ -0,0 +1,30 @@
+// Header
+
+
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int age = 10;
+			
+			while (age<18)
+			{
+				System.out.println("You are " + age + ". You cannot vote yet.");
+				age--;
+			}
+/*
+			age = 19;
+			do
+			{
+				System.out.println("You are " + age + ". You cannot vote yet.");
+				age++;
+			}while (age<18);
+*/
+			
+			
+			
+	}
+
+}
